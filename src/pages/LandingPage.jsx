cd4d5f2fbd52c561bf10d0510ec607bacff94750
yoginandal <yogidesign1996@gmail.com>
2024-12-01 04:27:28 +0530

    fixed bug

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,7 +5,7 @@ import AboutSection from "../layout/AboutSection";
 import SectionThree from "../layout/sectionThree";
 import TableSection from "../layout/TableSection";
 import WhyAttend from "../layout/WhyAttend";
-import Contact from "../layout/contact";
+import Contact from "../layout/Contact";
 export default function LandingPage() {
   return (
     <main>
