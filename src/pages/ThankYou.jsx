f4c11872723952b5a3345b247caba6543d5aca2a
yoginandal <yogidesign1996@gmail.com>
2024-12-01 04:33:35 +0530

    created thank you page

---
+++ src/pages/ThankYou.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/images/logo-new.webp";
+
+export default function ThankYouPage() {
+  const navigate = useNavigate();
+
+  const handleBackToHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-[#f63947] text-white text-center relative">
+      <img
+        className="w-[250px] p-4 mb-8 absolute top-0 left-0"
+        src={logo}
+        alt="Logo"
+      />
+      <div>
+        <h1 className="text-5xl font-bold mb-5 md:text-4xl">Thank You!</h1>
+        <p className="text-2xl mb-5 md:text-lg">
+          Your registration was successful. We will get in touch with you soon.
+        </p>
+        <button
+          className="bg-[#152a68] text-white border-none rounded px-5 py-3 text-lg font-bold cursor-pointer mt-5 md:text-sm"
+          onClick={handleBackToHome}
+        >
+          Back to Main Page
+        </button>
+      </div>
+    </div>
+  );
+}
